15b40cc9cd483688e3622b93a08cb612a74a3435
Likkzz <143434628+Likkzz@users.noreply.github.com>
2023-09-10 19:02:02 +0530

    Landing to  login

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
+# config/routes.rb
+
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-# config/routes.rb
-  resources :users  # This generates RESTful routes for UsersController
-  # ...
-  class UsersController < ApplicationController
-    # ... # Set the root page to the UsersController's index action
+  devise_scope :user do
+    root 'devise/sessions#new'
   end
-end 
+
+end
