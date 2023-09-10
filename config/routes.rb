bff3fb014ebdf62193d55fb8585f667927980303
Likkzz <143434628+Likkzz@users.noreply.github.com>
2023-09-10 13:11:36 +0530

    3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+# config/routes.rb
+  resources :users  # This generates RESTful routes for UsersController
+  # ...
+  class UsersController < ApplicationController
+    # ... # Set the root page to the UsersController's index action
+
+
 end
