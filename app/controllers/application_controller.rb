1298ad84c9076a956f3088fd8824aa158c6a290a
Likkzz <143434628+Likkzz@users.noreply.github.com>
2023-09-13 20:15:11 +0530

    Home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
   
     def after_sign_in_path_for(user)
-      # your path goes here
-      user_posts_path(user) # as an example
+      home_index_path
+      #user_posts_path(user) # as an example
     end
   
     
